f37ca9f489f1ad6b5b24603ac753f69cb0982e48
张美佳 <meijia1@staff.weibo.com>
2021-05-06 18:48:47 +0800

    源码分析

---
+++ spring-lagou/src/main/java/com/example/lagou/MyBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.example.lagou;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author MJ
+ * @date 2021/5/6
+ */
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+    public MyBeanPostProcessor() {
+        System.out.println("BeanPostProcessor 实现类构造函数...");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        if ("lagouBean".equals(beanName)) {
+            System.out.println("BeanPostProcessor 实现类 postProcessBeforeInitialization ⽅法被调⽤中......");
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        if ("lagouBean".equals(beanName)) {
+            System.out.println("BeanPostProcessor 实现类 postProcessAfterInitialization ⽅法被调⽤中......");
+        }
+        return bean;
+    }
+}
---
+++ spring-lagou/src/test/java/com/example/lagou/TestIoC.java
@@ -0,0 +1,20 @@
+package com.example.lagou;
+
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Ioc 容器源码分析基础案例
+ * @author MJ
+ * @date 2021/5/6
+ */
+public class TestIoC {
+    @Test
+    public void testIoC() {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        LagouBean lagouBean = applicationContext.getBean(LagouBean.class);
+        System.out.println(lagouBean);
+    }
+
+}
